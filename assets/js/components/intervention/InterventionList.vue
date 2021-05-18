1247e2449edeb42776578c3024bad38fe558a999
siwar12 <siwar.bousselmi@esprit.tn>
2021-05-18 11:59:27 +0100

    add CDatatable

--- assets/js/components/intervention/InterventionList.vue
+++ assets/js/components/intervention/InterventionList.vue
@@ -1,86 +1,105 @@
 <template>
-    <section id="main-content">
-        <section class="wrapper">
-            <h3><i class="fa fa-angle-right"></i> Gestion des interventions</h3>
-            <div class="row mt">
-                <div class="col-lg-12">
-                    <h4><i class="fa fa-angle-right"></i> Liste des interventions</h4>
-                    <div class="content-panel">
-                        <div class="adv-table" style="padding: 10px">
-                            <vue-table-dynamic
-                                    :params="params"
-                                    ref="table"
-                            >
-                            </vue-table-dynamic>
-                        </div>
-                    </div>
-                    <!-- /form-panel -->
-                </div>
-                <!-- /col-lg-12 -->
+  <section id="main-content">
+    <section class="wrapper">
+      <h3><i class="fa fa-angle-right"></i> Gestion des interventions</h3>
+      <div class="row mt">
+        <div class="col-lg-12">
+          <h4><i class="fa fa-angle-right"></i> Liste des interventions</h4>
+          <div class="content-panel">
+            <div class="adv-table" style="padding: 10px">
+              <CDataTable
+                  :items="items"
+                  :fields="fields"
+                  column-filter
+                  table-filter
+                  items-per-page-select
+                  :items-per-page="5"
+                  hover
+                  sorter
+                  pagination
+              >
+                <template #actions="{item, index}">
+                  <td class="py-2">
+                    <CButton
+                        color="primary"
+                        variant="outline"
+                        square
+                        size="sm"
+                    >
+                      {{Boolean(item._toggled) ? 'Hide' : 'Show'}}
+                    </CButton>
+                  </td>
+                </template>
+              </CDataTable>
             </div>
-            <!-- /row -->
-        </section>
+          </div>
+          <!-- /form-panel -->
+        </div>
+        <!-- /col-lg-12 -->
+      </div>
+      <!-- /row -->
     </section>
+  </section>
 </template>
 
 <script>
-    import VueTableDynamic from 'vue-table-dynamic'
-
-    export default {
-        name: "InterventionList",
-        components: { VueTableDynamic },
-        data() {
-            return {
-                params: {
-                    data: [
-                        ['ID', 'N°Intervention', `Date début`, `Structure`, `Observation(s)`, `Action(s)`]
-                    ],
-                    header: 'row',
-                    enableSearch: true,
-                    border: true,
-                    stripe: true,
-                    pagination: true,
-                    pageSize: 5,
-                    sort: [0, 1,2 ,3, 4, 5],
-                    pageSizes: [5, 10, 20, 50],
-                    columnWidth: [{column: 0, width: 100},{column: 1, width: 200},{column: 2, width: 200},{column: 3, width: 200},{column: 4, width: 1000},{column: 5, width: 400},],
-                    fixed: 0,
-                }
-
-            }
-        },
-        methods: {
-            list: function () {
-                const axios = require('axios');
-                let self = this;
-                // Make a request for a user with a given ID
-                axios.get('/api/interventions/list')
-                    .then(function (response) {
-                        // handle success
-                        self.interventions = response.data.interventionsByUser;
-                        for (let i = 0; i < self.interventions.length; i++) {
-                            let structure = self.interventions[i].agency.name + ' (' + self.interventions[i].agency.code + ') ';
-                            self.interventions[i].startDate.date = self.$options.filters.formatDate(self.interventions[i].startDate.date, 'fr');
-                            self.params.data.push(([self.interventions[i].id, self.interventions[i].number, self.interventions[i].startDate.date,structure, self.interventions[i].observation, self.addBtn()]))
-                        }
-                    });
-            },
-            addBtn: function(){
-                return ('<button type="button">Test</button>');
-            }
-        },
-        mounted() {
-            this.list()
-        },
+const fields = [
+  { key: 'id', _style:'min-width:100px' },
+  { key: 'number', _style:'min-width:100px;' },
+  { key: 'startDate', _style:'min-width:200px;' },
+  { key: 'agency', _style:'min-width:200px;' },
+  { key: 'observation', _style:'min-width:100px;' },
+  {
+    key: 'actions',
+    label: '',
+    _style: 'width:1%',
+    sorter: false,
+    filter: false,
+  }
+];
+export default {
+  name: "InterventionList",
+  components: { },
+  data() {
+    return {
+      fields,
+      details: [],
+      collapseDuration: 0,
+      items: [],
     }
+  },
+  methods: {
+    list: function () {
+      const axios = require('axios');
+      let self = this;
+      // Make a request for a user with a given ID
+      axios.get('/api/interventions/list')
+          .then(function (response) {
+            // handle success
+            self.items = response.data.interventionsByUser;
+            self.items.map((item) => {
+              item.startDate = self.$options.filters.formatDate(item.startDate.date, 'fr');
+              item.agency =  item.agency.name + '(' + item.agency.code + ')'
+              return {...item}})
+          });
+    },
+  },
+  mounted() {
+    this.list()
+  },
+  computed: {
+  },
+  watch: {
+  },
+}
 </script>
 
 <style scoped>
 .truncate{
-    width: 500px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
+  width: 500px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
 
