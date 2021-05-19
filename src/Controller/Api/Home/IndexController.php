1feb9da942d123e03df7619297d95ca8abafadd3
siwar-mae <siwar.bousselmi@esprit.tn>
2021-05-19 16:33:58 +0100

    interdire /back for user

--- src/Controller/Api/Home/IndexController.php
+++ src/Controller/Api/Home/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Api\Home;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class IndexController extends AbstractController
 {
@@ -12,6 +13,9 @@ class IndexController extends AbstractController
 
     /**
      * @Route("/home", name="home", options={"expose": true}, methods={"GET", "HEAD"})
+     *
+     * @IsGranted("ROLE_USER")
+     * @Security("is_granted('ROLE_USER')")
      */
     public function __invoke()
     {
