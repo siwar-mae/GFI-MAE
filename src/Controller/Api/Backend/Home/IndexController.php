64e0380193817c171659ddd7b1ac727a1e2e942a
siwar-mae <siwar.bousselmi@esprit.tn>
2021-05-19 16:47:04 +0100

    update

--- src/Controller/Api/Backend/Home/IndexController.php
+++ src/Controller/Api/Backend/Home/IndexController.php
@@ -1,14 +1,19 @@
 <?php
 
-namespace App\Controller\Api\Backend;
+namespace App\Controller\Api\Backend\Home;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/back", name="back", options={"expose": true}, methods={"GET", "HEAD"})
+     *
+     * @IsGranted("ROLE_ADMIN")
+     * @Security("is_granted('ROLE_ADMIN')")
      */
     public function __invoke()
     {
