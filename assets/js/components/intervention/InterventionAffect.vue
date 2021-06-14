e22d85f5afc63af1484d4290c7d4cec6c840b136
siwar-mae <siwar.bousselmi@esprit.tn>
2021-06-14 10:27:11 +0100

    add files

--- assets/js/components/intervention/InterventionAffect.vue
+++ assets/js/components/intervention/InterventionAffect.vue
@@ -60,77 +60,8 @@ export default {
   }
 };
 $(function() {
-
-  // /* initialize the external events
-  // -----------------------------------------------------------------*/
-  // $('#external-events .fc-event').each(function() {
-  //   // store data so the calendar knows to render an event upon drop
-  //   $(this).data('event', {
-  //     title: $.trim($(this).text()), // use the element's text as the event title
-  //     stick: true // maintain when user navigates (see docs on the renderEvent method)
-  //   });
-  //   // make the event draggable using jQuery UI
-  //   $(this).draggable({
-  //     zIndex: 999,
-  //     revert: true,      // will cause the event to go back to its
-  //     revertDuration: 0  //  original position after the drag
-  //   });
-  //
-  // });
-  // /* initialize the calendar
-  // -----------------------------------------------------------------*/
-  // $('#calendar').fullCalendar({
-  //   editable: true,
-  //   droppable: true, // this allows things to be dropped onto the calendar
-  //   dragRevertDuration: 0,
-  //   drop: function() {
-  //     // is the "remove after drop" checkbox checked?
-  //     if ($('#drop-remove').is(':checked')) {
-  //       // if so, remove the element from the "Draggable Events" list
-  //       $(this).remove();
-  //     }
-  //   },
-  //   eventDragStop: function( event, jsEvent, ui, view ) {
-  //     if(isEventOverDiv(jsEvent.clientX, jsEvent.clientY)) {
-  //       $('#calendar').fullCalendar('removeEvents', event._id);
-  //       var el = $( "<div class='fc-event'>" ).appendTo( '#external-events-listing' ).text( event.title );
-  //       el.draggable({
-  //         zIndex: 999,
-  //         revert: true,
-  //         revertDuration: 0
-  //       });
-  //       el.data('event', { title: event.title, id :event.id, stick: true });
-  //     }
-  //   },
-  //   eventReceive: function (event){
-  //     let self = this;
-  //     self.user = event.title;
-  //     self.date = event.start._d;
-  //     var affectation = new CustomEvent("affectation", {
-  //       "detail": {"user":self.user,"date":self.date }
-  //     });
-  //     document.dispatchEvent(affectation);
-  //   },
-  // });
-  //
-  // var isEventOverDiv = function(x, y) {
-  //   var external_events = $( '#external-events' );
-  //   var offset = external_events.offset();
-  //   offset.right = external_events.width() + offset.left;
-  //   offset.bottom = external_events.height() + offset.top;
-  //   // Compare
-  //   if (x >= offset.left
-  //       && y >= offset.top
-  //       && x <= offset.right
-  //       && y <= offset .bottom) { return true; }
-  //   return false;
-  // };
-
-
-
-  var calendarEl = document.getElementById('calendar');
-
-  var calendar = new FullCalendar.Calendar(calendarEl, {
+  let calendarEl = document.getElementById('calendar');
+  let calendar = new FullCalendar.Calendar(calendarEl, {
     plugins: [ 'dayGrid' ],
     defaultView: 'dayGridMonth',
     header: {
